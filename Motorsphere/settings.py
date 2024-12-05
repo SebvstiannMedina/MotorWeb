3960bf8e7c2a22957ba87ddaba0c336cbd5f8e11
SebvstiannMedina <seba.medina@duocuc.cl>
2024-12-05 18:26:47 -0300

    cambios

--- Motorsphere/settings.py
+++ Motorsphere/settings.py
@@ -119,6 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",  # Asegúrate de que el directorio "static" exista
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field

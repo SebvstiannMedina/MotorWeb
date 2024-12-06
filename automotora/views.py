b2862bdf5e6e524b5e75d1646e688be24aecc67f
SebvstiannMedina <seba.medina@duocuc.cl>
2024-12-05 22:35:23 -0300

    web lista

--- automotora/views.py
+++ automotora/views.py
@@ -3,17 +3,13 @@ from user_agents import parse
 import urllib.parse
 
 def index(request):
-    apk_url = 'https://github.com/SebvstiannMedina/apkmovil/raw/main/Motor Sphere.apk'
-    encoded_apk_url = urllib.parse.quote(apk_url)
+    apk_url = 'https://github.com/SebvstiannMedina/apkmovil/raw/main/Motor_Sphere.apk'
     
-    # Asegúrate de que el header HTTP_USER_AGENT existe
     user_agent_string = request.META.get('HTTP_USER_AGENT', '')
     user_agent = parse(user_agent_string)
     is_mobile = user_agent.is_mobile
     
     return render(request, 'automotora/index.html', {
-        'apk_url': encoded_apk_url,
+        'apk_url': apk_url,
         'is_mobile': is_mobile
     })
-
-# Puedes eliminar la función home si no la necesitas

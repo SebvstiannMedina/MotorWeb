3ddfd52ba9eaa61111d62836cc144f5187244cbf
SebvstiannMedina <seba.medina@duocuc.cl>
2024-12-05 21:33:12 -0300

    page lista

--- automotora/views.py
+++ automotora/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
-
-# Create your views here.
+from user_agents import parse
+import urllib.parse
 
 def index(request):
-    # Aquí va la lógica de tu vista
-    return render(request, 'automotora/index.html')
+    apk_url = 'https://github.com/SebvstiannMedina/apkmovil/raw/main/MotorSphere.apk'
+    encoded_apk_url = urllib.parse.quote(apk_url)
+    
+    # Asegúrate de que el header HTTP_USER_AGENT existe
+    user_agent_string = request.META.get('HTTP_USER_AGENT', '')
+    user_agent = parse(user_agent_string)
+    is_mobile = user_agent.is_mobile
+    
+    return render(request, 'automotora/index.html', {
+        'apk_url': encoded_apk_url,
+        'is_mobile': is_mobile
+    })
+
+# Puedes eliminar la función home si no la necesitas
